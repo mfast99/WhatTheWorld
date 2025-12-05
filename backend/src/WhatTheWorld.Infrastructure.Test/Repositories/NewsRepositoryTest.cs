98291bfa8dfc6d459588d10999cb44128f660d36
mfast99 <mikefast@gmx.net>
2025-12-05 10:55:58 +0100

    Test

---
+++ backend/src/WhatTheWorld.Infrastructure.Test/Repositories/NewsRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using WhatTheWorld.Domain;
+using WhatTheWorld.Infrastructure.Data;
+using WhatTheWorld.Infrastructure.Repositories;
+using Xunit;
+
+namespace WhatTheWorld.Infrastructure.Test.Repositories
+{
+    public class NewsRepositoryTest
+    {
+    }
+}
---
+++ backend/src/WhatTheWorld.Infrastructure.Test/Repositories/WeatherRepositoryTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatTheWorld.Infrastructure.Test.Repositories
+{
+    internal class WeatherRepositoryTest
+    {
+    }
+}
